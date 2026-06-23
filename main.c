5be33e22805ff6ebc79befa8010dfc514f357519
Sunhaloo <azmaan151003@gmail.com>
2026-06-23 17:19:57 +0400

    U: Completed First Tutorial Video
    So... This is basicaly the "Hello World" program of Graphics Programming
    ( x_x)

--- main.c
+++ main.c
@@ -1,16 +1,71 @@
 // own the function implementation found in sokol's header file
-#define SOKOL_APP_IMPL
+#define SOKOL_IMPL
 // Linux: using OpenGL's API to communicate with GPU
 #define SOKOL_GLCORE
 // NOTE: please read line 1154 of "our" `sokol_app.h` header file
 #define SOKOL_NO_ENTRY
-// include the actual sokol header file
+// include the sokol header file --> windowing and events
 #include "dependencies/sokol/sokol_app.h"
+// include the sokol header file --> simple GPU API wrapper - pixels, rendering
+#include "dependencies/sokol/sokol_gfx.h"
+// include the sokol header file --> helper functions for 'sokol_gfx.h' file
+#include "dependencies/sokol/sokol_glue.h"
+
+// state stucture for rendering
+static struct {
+  // action performed during a render pass
+  sg_pass_action pass_action;
+} state;
 
 // function related to `sapp_run` and `sapp_desc`
-void init(void) {};
-void frame(void) {};
-void cleanup(void) {};
+void init(void) {
+  // function to handle initialisation for window system and GPU rendering
+
+  // initialise graphics for rendering ==> preparing memory stuff and pipelines
+  sg_setup(&(sg_desc){
+      // setup the environment ==> see line 5006 in 'sokol_gfx.h' for `struct`
+      .environment = sglue_environment()});
+
+  // update the state
+  // INFO: again my formatter is really weird WTF is this?
+  state.pass_action =
+      (sg_pass_action){              // render pass to have colours
+                       .colors[0] = {// clean the screen from whatever we have
+                                     .load_action = SG_LOADACTION_CLEAR,
+                                     // change the colour
+                                     .clear_value = {
+                                         // red colour
+                                         0.35f,
+                                         // green colour
+                                         0.35f,
+                                         // blue colour
+                                         0.35f,
+                                         // opacity
+                                         1.0f,
+                                     }}};
+};
+
+void frame(void) {
+  // function to display at each render state ==> called once every frame
+
+  // start the pass to display at each state
+  sg_begin_pass(
+      &(sg_pass){.action = state.pass_action, .swapchain = sglue_swapchain()});
+
+  // finish recording commands for this pass
+  sg_end_pass();
+
+  // submit / "write" all command to the GPU
+  sg_commit();
+};
+
+void cleanup(void) {
+  // function to cleanup resources at the end of our program
+
+  // shutdown / kill the instance of our sokol graphics
+  sg_shutdown();
+};
+
 void event(const sapp_event *event) {};
 
 // our main function
